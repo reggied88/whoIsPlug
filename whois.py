cbd5c212005c8e7f128fdf4490f7b9b38a56f159
Reggie Davis <reggied@blinds.com>
2017-11-24 16:43:25 -0600

    Added exception

--- whois.py
+++ whois.py
@@ -5,7 +5,14 @@ class WhoIsDomain(BotPlugin):
     """This plugin allows a user to return the whois info of a domain"""
     err_version = '5.1.3'
 
+    def activate(self):
+        super(WhoIsDomain, self).activate()
+
+	try:
+		Popen(['whois'], stdout=PIPE, stderr=STDOUT).communicate()
+	except Exception as e:
+		self.warn_admins("This plugin uses whois and it wasn't found on your system") 
+
     @botcmd
     def whois(self, msg, args):
-        """Say whats up to the world"""
         return Popen(['whois'] + args.split, stdout=PIPE, STDOUT).communicate()
